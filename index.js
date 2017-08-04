ddd7bcc612a122473abeeecc8d123f6a925cf645
Quentin Bramas <quentin.bramas@gmail.com>
2017-08-04 23:57:05 +0200

    port from env

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.post('/build', async function (req, res) {
   res.send(JSON.stringify(result, null, 2));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Example app listening on port '+port+'!');
 });
