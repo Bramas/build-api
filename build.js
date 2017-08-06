109959fff87bda637267902922198c02f21d52dd
root <root@vps344337.ovh.net>
2017-08-06 22:52:30 +0200

    accept url containing a zip of the sources to build

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ const util = require('util');
 const child_process = require('child_process');
 const fs = require('fs-extra');
 
-const RUN = 'docker run --name {2} --rm -v $PWD/tmp/{1}:/var/src buildapi';
+const RUN = 'docker run --name {2} --rm -v {1}:/var/src buildapi';
 
 function exec(cmd) {
   return new Promise((acc, rej) => {
@@ -15,47 +15,50 @@ function exec(cmd) {
     });
   });
 }
+
+async function runDockerCommand(cmd, workingDirectory, timeout) {
+
+  const nonce = (await crypto.randomBytes(25)).toString('hex');
+  const containerName = 'buildapi_'+nonce;
+  const runCmd =  RUN.replace(/\{1\}/g, workingDirectory).replace(/\{2\}/g, containerName);
+
+  var error = {};
+  var timoutId = setTimeout(() => {
+    exec('docker kill '+containerName);
+    timoutId = null;
+    error =  {
+      timeout: timeout,
+      message: "The process has been killed after "+timeout+" seconds"
+    }
+  }, timeout*1000);
+
+  const result = await exec(runCmd+' '+cmd);
+  if(timoutId) clearTimeout(timoutId);
+
+  if(result.error)
+    error = Object.assign(result.error, error);
+  // include possible timeout error
+  if(error != {}) {
+    result.error = error;
+  }
+  // remove empty stderr
+  if(!result.stderr) {
+    delete result.stderr;
+  }
+  // remove empty error
+  if(!result.error) {
+    delete result.error;
+  }
+  result.command = cmd;
+  return result;
+}
+
 /**
 * @param timeout in second, for the execution of the program
 */
-module.exports = async function(timeout) {
-  const nonce = (await crypto.randomBytes(25)).toString('hex');
-  const tmpWD = __dirname+'/tmp/'+nonce;
-  const containerName = 'buildapi_'+nonce;
-  await fs.copy(__dirname+'/template', tmpWD);
-
-  const runCmd =  RUN.replace(/\{1\}/g, nonce).replace(/\{2\}/g, containerName);
-  console.log(runCmd+' make hello');
-  let result = {}
-  var timoutId = setTimeout(() => {
-    exec('docker kill '+containerName);
-    console.log('KIIILL COMPILATION ','docker kill '+containerName);
-    timoutId = null;
-    result.compilation = {
-      error: {
-        timeout: 60,
-        message: "The compilation of your program has been killed after "+60+" seconds"
-      }
-    }
-  }, 60000);
-  result.compilation = Object.assign(await exec(runCmd+' make hello'), result.compilation || {});
-  if(timoutId) clearTimeout(timoutId);
-  console.log('make:', result.compilation);
-  console.log(runCmd+' "./hello"');
-  timoutId = setTimeout(() => {
-    exec('docker kill '+containerName);
-    console.log('KIIILL EXECUTION','docker kill '+containerName);
-    result.execution = {
-      error: {
-        timeout,
-        message: "Your program has been killed after "+timeout+" seconds"
-      }
-    }
-    timoutId = null;
-  }, timeout*1000);
-  result.execution = Object.assign(await exec(runCmd+' "./hello"'), result.execution || {});
-  if(timoutId) clearTimeout(timoutId);
-  console.log('exec:', result.execution);
-  await fs.remove(tmpWD);
+module.exports = async function(tmpWD, timeout) {
+  const result = {};
+  result.compilation = await runDockerCommand('make hello', tmpWD, 60);
+  result.execution = await runDockerCommand('./hello', tmpWD, timeout);
   return result;
 }
--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const app = express();
 const build = require('./build');
+const request = require('request');
+const unzip = require('unzip-stream');
+const fs = require('fs-extra');
+const crypto = require('crypto');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at:', p, 'reason:', reason);
+  // application specific logging, throwing an error, or other logic here
+});
+
 app.post('/build', async function (req, res) {
   console.log(req.body);
-  const result = await build(3);
-  res.send(JSON.stringify(result, null, 2));
+  if(req.body.url) {
+  	const nonce = (await crypto.randomBytes(25)).toString('hex');
+  	const tmppath=__dirname+'/tmp/'+nonce;
+	request.get(req.body.url)
+	.pipe(unzip.Extract({ path: tmppath }))
+	.on('close', async function() {
+		console.log('END');
+		const result = await build(tmppath, 3);
+  		await fs.remove(tmppath);
+  		res.send(JSON.stringify(result, null, 2));
+	}).on('error', function(e) {
+		console.error(e);
+		res.end('Erreur');
+	});
+  }
+
+
 });
 
 const port = process.env.PORT || 3000;
